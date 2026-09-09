961a24eae6b000e46831ad7356640d2dde4a694c
Sigit Adi <145288379+sisigitadi@users.noreply.github.com>
2026-09-09 14:32:49 +0700

    fix: disable image optimization for faster external image loading

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
